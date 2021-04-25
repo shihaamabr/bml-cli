47b316ca6081d570feee0cbabe520ddcedb0bdd5
Shihaam Abdul Rahman <shihaam_ab_r@outlook.com>
2021-04-25 15:16:00 +0500

    Added hint, to do

--- pasrjsontest.sh
+++ pasrjsontest.sh
@@ -11,4 +11,7 @@ curl -s -c $COOKIE $BML_URL/login \
 curl -s -b $COOKIE $BML_URL/profile > /dev/null
 #curl -s -b $COOKIE $BML_URL/contacts | jq -r '["Account Number","Currency","Account Name","Contact Name"], (.["payload"] | .[] | [.account, .currency, .name, .alias]) | @tsv' contacts.json
 #curl -s -b $COOKIE $BML_URL/validate/account/7704265806101
+
+
+#get this to render nicely in a table, similar to contacts
 curl -s -b $COOKIE $BML_URL/dashboard
