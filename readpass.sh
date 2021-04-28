87fdd64328ce3e2c77f8b0014f8312782fa53946
Shihaam Abdul Rahman <github@shihaam.me>
2021-04-28 07:15:26 +0500

    password saving

--- readpass.sh
+++ readpass.sh
@@ -1,29 +1,24 @@
-
 if [ -f $CREDENTIALS ]
 then
 	source $CREDENTIALS
-elif [ "$LOGIN" = "2" ]
-then
-	if [ -f $CREDENTIALS ]
+	if [ "$LOGIN" = "2" ]
 	then
-		echo "${red}Saved Credentials has been changed${reset}"
-		rm $CREDENTIALS
+		echo "${red}Password update required${reset}"
+		rm $CREDENTIALS 2> /dev/null
+		read -p 'Username: ' BML_USERNAME
+		read -s -p 'Password: ' BML_PASSWORD
+		echo ""
 	else
-		echo "${red}Username or Password incorrect${reset}"
+		:
 	fi
-	read -p 'Username: ' BML_USERNAME
-	read -s -p 'Password: ' BML_PASSWORD
-	echo ""
-
 elif [ ! -f $CREDENTIALS ]
 then
-	echo "${red}Password file not found in $CREDENTIALS${reset}"
+	echo "${red}Username or Password in correct${reset}"
 	read -p 'Username: ' BML_USERNAME
 	read -s -p 'Password: ' BML_PASSWORD
 	echo ""
 else
 	:
 fi
-
 
 source login.sh
