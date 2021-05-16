9a6e24450a510b5d7a00b8b09517f961196de6e7
Shihaam Abdul Rahman <github@shihaam.me>
2021-05-17 00:31:44 +0500

    rewrite

--- listcontacts.sh
+++
@@ -1,3 +0,0 @@
-curl -s -b $COOKIE $BML_URL/contacts \
-| jq -r '["ID","Account Number","Currency","Account Name","Contact Name"], ["==================================================================="], (.["payload"] | .[] | [.id, .account, .currency, .name, .alias]) | @tsv'
-source contactsmenu.sh
