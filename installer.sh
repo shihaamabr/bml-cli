79e6f9a4c18abbf8b92ad7ab09b4e1cb1f88ec76
Shihaam Abdul Rahman <github@shihaam.me>
2021-05-21 03:48:24 +0500

    packing

--- installer.sh
+++ installer.sh
@@ -4,7 +4,7 @@ git clone https://github.com/shihaamabr/bml-cli.git
 mkdir -p /opt/sar/bml-cli/
 mv bml-cli/* /opt/sar/bml-cli/
 echo "#!/bin/bash" > /usr/bin/bml-cli
-echo "source /opt/sar/bml-cli/bml-cli.sh" >> /usr/bin/bml-cli
+echo "source /opt/sar/bml-cli/bml-cli" >> /usr/bin/bml-cli
 chmod 755 /usr/bin/bml-cli
 rm -rf bml-cli/
 echo "Installation Complete"
