9bee61cd10a3802e55430a5d9c5e17f79e287079
Shihaam Abdul Rahman <github@shihaam.me>
2021-06-01 09:37:41 +0500

    installer dependecies check debian

--- installer.sh
+++ installer.sh
@@ -1,4 +1,32 @@
 #!/bin/bash
+echo "Checking for curl..."
+if command -v curl > /dev/null; then
+echo "Detected curl..."
+else
+echo "Installing curl"
+apt-get install -y gnupg
+
+echo "Checking for jq..."
+if command -v jq > /dev/null; then
+echo "Detected jq..."
+else
+echo "Installing jq"
+apt-get install -y jq
+
+echo "Checking for openssl..."
+if command -v openssl > /dev/null; then
+echo "Detected openssl"
+else
+echo "Installing openssl"
+apt-get install -y openssl
+
+echo "Checking for perl..."
+if command -v perl > /dev/null; then
+echo "Detected perl..."
+else
+echo "Installing perl"
+apt-get install -y perl
+
 cd /tmp/
 git clone https://github.com/shihaamabr/bml-cli.git
 mkdir -p /opt/sar/bml-cli/
